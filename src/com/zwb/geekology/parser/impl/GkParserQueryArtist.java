a17567ca3372588472e336dac54b24a27c55f5d4
mrsfoo <mrs.foo@posteo.de>
2014-09-08 23:23:08 +0200

    album name "st" translation

--- src/com/zwb/geekology/parser/impl/GkParserQueryArtist.java
+++ src/com/zwb/geekology/parser/impl/GkParserQueryArtist.java
@@ -4,52 +4,55 @@ import com.zwb.geekology.parser.api.parser.IGkParserQuery;
 
 public class GkParserQueryArtist implements IGkParserQuery
 {
-	private String artistName;
-	private String releaseName;
-	
-	public GkParserQueryArtist(String artistName) 
+    private String artistName;
+    private String releaseName;
+    
+    public GkParserQueryArtist(String artistName)
+    {
+	this.artistName = artistName;
+    }
+    
+    public GkParserQueryArtist(String artistName, String releaseName)
+    {
+	this.artistName = artistName;
+	this.releaseName = releaseName;
+    }
+    
+    public String toString()
+    {
+	String s = "q(a):" + this.artistName;
+	if (this.hasRelease())
 	{
-		this.artistName = artistName;
+	    s += "/" + this.releaseName;
 	}
-	
-	public GkParserQueryArtist(String artistName, String releaseName) 
+	return s;
+    }
+    
+    @Override
+    public String getArtist()
+    {
+	return this.artistName;
+    }
+    
+    @Override
+    public String getRelease()
+    {
+	if (GeneralConfig.ST_IDENTIFIERS.contains(this.releaseName))
 	{
-		this.artistName = artistName;
-		this.releaseName = releaseName;
+	    return this.artistName;
 	}
-	
-	public String toString()
-	{
-		String s = "q(a):"+this.artistName;
-		if(this.hasRelease())
-		{
-			s += "/"+this.releaseName;
-		}
-		return s;
-	}
-	
-	@Override
-	public String getArtist() 
-	{
-		return this.artistName;
-	}
-
-	@Override
-	public String getRelease() 
-	{
-		return this.releaseName;
-	}
-
-	@Override
-	public boolean hasRelease()
-	{
-		return this.releaseName!=null;
-	}
-
-	@Override
-	public boolean isSampler()
-	{
-		return false;
-	}
+	return this.releaseName;
+    }
+    
+    @Override
+    public boolean hasRelease()
+    {
+	return this.releaseName != null;
+    }
+    
+    @Override
+    public boolean isSampler()
+    {
+	return false;
+    }
 }
-
